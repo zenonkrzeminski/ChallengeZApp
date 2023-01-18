26120f876c1389d3c6ea46588a8b974e988e8ba6
Zenon Krzemiński <krzeminskizp@gmail.com>
2023-01-18 16:01:46 +0100

    Zadanie - Dzień 4

--- ChallengeZApp/ChallengeZApp/Program.cs
+++ ChallengeZApp/ChallengeZApp/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program sprawdzający płeć, wiek i pełnoletność.
+// Na koniec kawlifikująca do jednej z grup:
+// (1) osób pełnoletnich poniżej 30 lat "z rozróżnieniem płci"
+// (2) osób pełnoletnich powyżej 30 lat "z podaniem imienia i wieku"
+// (3) osób niepełnoletnim "z rozróżnieniem płci"
+
+// Wprowadzone dane:
+string name = "Eddard";
+string gender = "Mężczyzna";
+int age = 54;
+
+Console.WriteLine("Osoba kwalifikuje się do grupy:");
+
+if (age >= 30)
+{
+    Console.WriteLine($"(2): \"{name}, lat {age}\"");
+}
+else if (age < 18)
+{
+    if (gender == "Mężczyzna")
+    {
+        Console.WriteLine($"(3): \"Niepełnoletni mężczyzna\"");
+    }
+    else
+    {
+        Console.WriteLine($"(3): \"Niepełnoletnia kobieta\"");
+    }
+}
+else
+{
+    if (gender == "Mężczyzna")
+    {
+        Console.WriteLine($"(1): \"Mężczyzna poniżej 30 lat\"");
+    }
+    else
+    {
+        Console.WriteLine($"(1): \"Kobieta poniżej 30 lat\"");
+    }
+}
